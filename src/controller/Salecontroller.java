666bc599f4e1164437ce7d81cd20049c21c6c005
Mai Thanh Tùng <maithanhtung251212@gmail.com>
2026-05-19 23:17:49 +0700

    Khoi tao du an Miniti

---
+++ src/controller/Salecontroller.java
@@ -0,0 +1,6 @@
+package controller;
+
+public class Salecontroller {
+    
+}
+// code kiểm tra kết nối CSDL, thêm sửa xóa nhân viên, thêm sửa xóa sản phẩm, hiển thị danh sách nhân viên, hiển thị danh sách sản phẩm
---
+++ src/vỉew/Login.java
@@ -0,0 +1,7 @@
+// giao diện đăng nhập, cho phép người dùng nhập tên đăng nhập và mật khẩu để truy cập vào phần mềm
+
+package vỉew;
+
+public class Login {
+    
+}
