0898a430bf494db560231853fe0af8bd14207561
Bao <bao@gmail.com>
2026-05-20 11:00:06 +0700

    Tao them cac file moi cho du an Miniti

--- src/model/Sanpham.java
+++ src/model/Sanpham.java
@@ -1,5 +1,67 @@
 package model;
 
 public class Sanpham {
-    
+
+    private String maSP;
+    private String tenSP;
+    private int soLuong;
+    private double giaBan;
+
+    // Constructor rỗng
+    public Sanpham() {
+    }
+
+    // Constructor đầy đủ
+    public Sanpham(String maSP, String tenSP, int soLuong, double giaBan) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.soLuong = soLuong;
+        this.giaBan = giaBan;
+    }
+
+    // GETTER & SETTER
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    // toString()
+
+    @Override
+    public String toString() {
+        return "Sanpham{" +
+                "maSP='" + maSP + '\'' +
+                ", tenSP='" + tenSP + '\'' +
+                ", soLuong=" + soLuong +
+                ", giaBan=" + giaBan +
+                '}';
+    }
 }
--- src/view/Mainframe.java
+++ src/view/Mainframe.java
@@ -1,6 +1,5 @@
 
 // giao diện chính của phần mềm, hiển thị các chức năng chính như quản lý nhân viên, quản lý sản phẩm, quản lý hóa đơn, báo cáo doanh thu, v.v.
-
 package vỉew;
 
 public class Mainframe {
