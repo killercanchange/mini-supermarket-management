33f2ea92f5abbc596d710c5b70135139e56ec56b
Bao <bao@gmail.com>
2026-05-20 11:05:45 +0700

    Tao them cac file moi cho du an Miniti

--- src/model/HoaDon.java
+++ src/model/HoaDon.java
@@ -5,68 +5,105 @@ import java.time.LocalDate;
 public class HoaDon {
 
     private String maHD;
-    private String maNV; // Mã nhân viên lập hóa đơn
-    private String maKH; // Mã khách hàng (có thể null nếu khách lẻ)
-    private LocalDate ngayTao;
+    private String maSP;
+    private String tenSP;
+    private int soLuongMua;
+    private double giaBan;
     private double tongTien;
-    private String trangThai; // Ví dụ: "Đã thanh toán", "Chưa thanh toán"
-    private String ghiChu;
+    private LocalDate ngayLap;
 
     // Constructor rỗng
-    public HoaDon() {}
+    public HoaDon() {
+    }
 
     // Constructor đầy đủ
-    public HoaDon(String maHD, String maNV, String maKH, LocalDate ngayTao, 
-                  double tongTien, String trangThai, String ghiChu) {
+    public HoaDon(String maHD, String maSP, String tenSP,
+                  int soLuongMua, double giaBan,
+                  LocalDate ngayLap) {
+
         this.maHD = maHD;
-        this.maNV = maNV;
-        this.maKH = maKH;
-        this.ngayTao = ngayTao;
-        this.tongTien = tongTien;
-        this.trangThai = trangThai;
-        this.ghiChu = ghiChu;
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.soLuongMua = soLuongMua;
+        this.giaBan = giaBan;
+        this.ngayLap = ngayLap;
+
+        // tự tính tổng tiền
+        this.tongTien = soLuongMua * giaBan;
     }
 
     // GETTER & SETTER
 
-    public String getMaHD() { return maHD; }
-    public void setMaHD(String maHD) { this.maHD = maHD; }
-
-    public String getMaNV() { return maNV; }
-    public void setMaNV(String maNV) { this.maNV = maNV; }
-
-    public String getMaKH() { return maKH; }
-    public void setMaKH(String maKH) { this.maKH = maKH; }
-
-    public LocalDate getNgayTao() { return ngayTao; }
-    public void setNgayTao(LocalDate ngayTao) { this.ngayTao = ngayTao; }
-
-    public double getTongTien() { return tongTien; }
-    public void setTongTien(double tongTien) { this.tongTien = tongTien; }
-
-    public String getTrangThai() { return trangThai; }
-    public void setTrangThai(String trangThai) { this.trangThai = trangThai; }
-
-    public String getGhiChu() { return ghiChu; }
-    public void setGhiChu(String ghiChu) { this.ghiChu = ghiChu; }
-
-    // PHƯƠNG THỨC TIỆN ÍCH 
-
-    // Kiểm tra xem hóa đơn đã được thanh toán hay chưa
-    public boolean isDaThanhToan() {
-        return "Đã thanh toán".equalsIgnoreCase(this.trangThai) || 
-               "Da thanh toan".equalsIgnoreCase(this.trangThai);
+    public String getMaHD() {
+        return maHD;
     }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public int getSoLuongMua() {
+        return soLuongMua;
+    }
+
+    public void setSoLuongMua(int soLuongMua) {
+        this.soLuongMua = soLuongMua;
+
+        // cập nhật lại tổng tiền
+        this.tongTien = this.soLuongMua * this.giaBan;
+    }
+
+    public double getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+
+        // cập nhật lại tổng tiền
+        this.tongTien = this.soLuongMua * this.giaBan;
+    }
+
+    public double getTongTien() {
+        return tongTien;
+    }
+
+    public LocalDate getNgayLap() {
+        return ngayLap;
+    }
+
+    public void setNgayLap(LocalDate ngayLap) {
+        this.ngayLap = ngayLap;
+    }
+
+    // toString()
 
     @Override
     public String toString() {
         return "HoaDon{" +
                 "maHD='" + maHD + '\'' +
-                ", maNV='" + maNV + '\'' +
-                ", maKH='" + maKH + '\'' +
-                ", ngayTao=" + ngayTao +
+                ", maSP='" + maSP + '\'' +
+                ", tenSP='" + tenSP + '\'' +
+                ", soLuongMua=" + soLuongMua +
+                ", giaBan=" + giaBan +
                 ", tongTien=" + tongTien +
-                ", trangThai='" + trangThai + '\'' +
+                ", ngayLap=" + ngayLap +
                 '}';
     }
 }
